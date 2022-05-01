fcf0192fed966d5c978d4f3c9acde1ea66a7cdc0
Javier Fornells Calvo <fornelc@gmail.com>
2022-05-01 19:15:52 +0200

    Data Structures: Added comment for QuickSort exercise

--- src/main/java/com/algorithms/datastructures/sorting/QuickSort.java
+++ src/main/java/com/algorithms/datastructures/sorting/QuickSort.java
@@ -1,5 +1,19 @@
 package com.algorithms.datastructures.sorting;
 
+/**
+ * The worst case scenario is gonna be O(n^2) time, this is gonna happen when each time that you cut in half your array,
+ * the smaller subArray has a length of one, and the other side is going to contain the rest of elements of the array,
+ * so you are going to end iterating the left and right pointers n^2 times.
+ *
+ * The average and the best are going to be O(nlog(n)), because in average we will be cutting the input in half,
+ * and we will be doing each time O(log(n)) time operations before you reach sub arrays of length one.
+ *
+ * Space complexity O(log(n)), as we will be sorting first the smaller subArray,
+ * the smaller subArray is going to raise a stack frame of O(log(n)) space.
+ * When afterwards we start to sort the bigger subArray,
+ * it will be sort using tail recursion which doesn't add to the call stack,
+ * keeping the stack bounded to O(log(n))
+ */
 public class QuickSort {
     public static int[] quickSort(int[] array) {
         quickSortHelper(0, array.length - 1, array);
